98948a38b3566a5826c05fed51d64dd380c54a53
Hugofsp93 <hugofsp93@hotmail.com>
2025-04-22 15:58:14 -0300

    Lots of styling and global tailwind colors in application.css

--- app/frontend/layouts/App.jsx
+++ app/frontend/layouts/App.jsx
@@ -18,46 +18,46 @@ export default function App({ children }) {
     <>
       <Head title="App" />
       {/* more navbar examples: https://flowbite.com/docs/components/navbar/ */}
-      <nav className="bg-white dark:bg-zinc-950 sticky w-full z-20 top-0 start-0 border-b border-zinc-200 dark:border-zinc-700">
+      <nav className="bg-white dark:bg-neutral-950 sticky w-full z-20 top-0 start-0 border-b border-neutral-200 dark:border-neutral-700">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={RubyLogo} className="h-8" alt="Rails Template" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-zinc-900 dark:text-white">Rails Template</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-neutral-900 dark:text-white">Rails Template</span>
           </a>
           <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <ThemeToggle />
-            <button type="button" className="flex text-sm bg-white dark:bg-zinc-950 rounded-full md:me-0 focus:ring-2 focus:ring-zinc-300 dark:focus:ring-zinc-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+            <button type="button" className="flex text-sm bg-white dark:bg-neutral-950 rounded-full md:me-0 focus:ring-2 focus:ring-neutral-300 dark:focus:ring-neutral-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
               <span className="sr-only">Open user menu</span>
               <img className="w-8 h-8 rounded-full" src={FlowbiteLogo} alt="user photo" />
             </button>
-            <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-zinc-100 rounded-lg shadow-sm dark:bg-zinc-700 dark:divide-zinc-600" id="user-dropdown">
+            <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-neutral-100 rounded-lg shadow-sm dark:bg-neutral-700 dark:divide-neutral-600" id="user-dropdown">
               <div className="px-4 py-3">
-                <span className="block text-sm text-zinc-900 dark:text-white">Bonnie Green</span>
-                <span className="block text-sm  text-zinc-500 truncate dark:text-zinc-400">{currentUser.email}</span>
+                <span className="block text-sm text-neutral-900 dark:text-white">Bonnie Green</span>
+                <span className="block text-sm  text-neutral-500 truncate dark:text-neutral-400">{currentUser.email}</span>
               </div>
               <ul className="py-2" aria-labelledby="user-menu-button">
                 <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-zinc-700 hover:bg-zinc-100 dark:hover:bg-zinc-600 dark:text-zinc-200 dark:hover:text-white">Dashboard</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-600 dark:text-neutral-200 dark:hover:text-white">Dashboard</a>
                 </li>
                 <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-zinc-700 hover:bg-zinc-100 dark:hover:bg-zinc-600 dark:text-zinc-200 dark:hover:text-white">Settings</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-600 dark:text-neutral-200 dark:hover:text-white">Settings</a>
                 </li>
                 <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-zinc-700 hover:bg-zinc-100 dark:hover:bg-zinc-600 dark:text-zinc-200 dark:hover:text-white">Earnings</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-600 dark:text-neutral-200 dark:hover:text-white">Earnings</a>
                 </li>
                 <li>
                   <Link
                     href="/users/sign_out"
                     method="delete"
                     as="button"
-                    className="block w-full text-left px-4 py-2 text-sm text-zinc-700 hover:bg-zinc-100 dark:hover:bg-zinc-600 dark:text-zinc-200 dark:hover:text-white"
+                    className="block w-full text-left px-4 py-2 text-sm text-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-600 dark:text-neutral-200 dark:hover:text-white"
                   >
                     Sign Out
                   </Link>
                 </li>
               </ul>
             </div>
-            <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-zinc-500 rounded-lg md:hidden hover:bg-zinc-100 focus:outline-none focus:ring-2 focus:ring-zinc-200 dark:text-zinc-400 dark:hover:bg-zinc-700 dark:focus:ring-zinc-600" aria-controls="navbar-user" aria-expanded="false">
+            <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-neutral-500 rounded-lg md:hidden hover:bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-neutral-200 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:ring-neutral-600" aria-controls="navbar-user" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -65,28 +65,28 @@ export default function App({ children }) {
             </button>
           </div>
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-zinc-100 rounded-lg bg-zinc-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-zinc-950 md:dark:bg-zinc-950 dark:border-zinc-800">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-neutral-100 rounded-lg bg-neutral-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-neutral-950 md:dark:bg-neutral-950 dark:border-neutral-800">
               <li>
-                <a href="#" className="block py-2 px-3 text-white bg-red-700 rounded-sm md:bg-transparent md:text-red-700 md:p-0 md:dark:text-red-400" aria-current="page">Home</a>
+                <a href="/" className="block py-2 px-3 text-neutral-900 rounded-sm hover:bg-transparent hover:text-utility-700 md:p-0 dark:text-white dark:hover:text-utility-400">Home</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-zinc-900 rounded-sm hover:bg-zinc-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-zinc-800">About</a>
+                <a href="#" className="block py-2 px-3 text-neutral-900 rounded-sm hover:bg-transparent hover:text-utility-700 md:p-0 dark:text-white dark:hover:text-utility-400">About</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-zinc-900 rounded-sm hover:bg-zinc-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-zinc-800">Services</a>
+                <a href="#" className="block py-2 px-3 text-neutral-900 rounded-sm hover:bg-transparent hover:text-utility-700 md:p-0 dark:text-white dark:hover:text-utility-400">Services</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-zinc-900 rounded-sm hover:bg-zinc-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-zinc-800">Pricing</a>
+                <a href="#" className="block py-2 px-3 text-neutral-900 rounded-sm hover:bg-transparent hover:text-utility-700 md:p-0 dark:text-white dark:hover:text-utility-400">Pricing</a>
               </li>
               <li>
-                <a href="/contact" className="block py-2 px-3 text-zinc-900 rounded-sm hover:bg-zinc-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-zinc-800">Contact</a>
+                <a href="/contact" className="block py-2 px-3 text-neutral-900 rounded-sm hover:bg-transparent hover:text-utility-700 md:p-0 dark:text-white dark:hover:text-utility-400">Contact</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      <div className="flex flex-col min-h-[calc(100vh-4rem)] bg-[#fff6f6] dark:bg-zinc-900">
+      <div className="flex flex-col min-h-[calc(100vh-4rem)] bg-utility-50 dark:bg-neutral-900">
         <main className="flex-1 flex flex-col items-center p-10">
           {children}
         </main>
--- app/frontend/pages/auth/ResetPassword.jsx
+++ app/frontend/pages/auth/ResetPassword.jsx
@@ -64,59 +64,59 @@ export default function ResetPassword() {
       {toast && <Toast {...toast} />}
       <section className="w-full mt-20 max-w-lg bg-transparent">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-          <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-zinc-950 dark:border-zinc-800 sm:p-8">
-            <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-zinc-900 md:text-2xl dark:text-white">
+          <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-neutral-950 dark:border-neutral-800 sm:p-8">
+            <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-neutral-900 md:text-2xl dark:text-white">
               Change Password
             </h2>
             <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" onSubmit={handleSubmit} noValidate>
               <div className="h-[85px]">
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-zinc-900 dark:text-white">Your email</label>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-neutral-900 dark:text-white">Your email</label>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  className="bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-zinc-700 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                  className="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-neutral-600 focus:border-neutral-600 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-700 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-utility-500 dark:focus:border-utility-500"
                   placeholder={email}
                   disabled
                   required
                 />
               </div>
               <div className="h-[85px]">
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-zinc-900 dark:text-white">New Password</label>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-neutral-900 dark:text-white">New Password</label>
                 <input 
                   type="password" 
                   name="password" 
                   id="password" 
                   onChange={(e) => handleChange('password', e.target.value)} 
                   placeholder="••••••••" 
-                  className={`bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg block w-full p-2.5 dark:bg-zinc-700 dark:text-white dark:border-zinc-700 ${
-                    errors.password ? 'border-red-500 focus:ring-red-500 focus:border-red-500' : 'focus:ring-red-500 focus:border-red-500'
+                  className={`bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg block w-full p-2.5 dark:bg-neutral-700 dark:text-white dark:border-neutral-700 ${
+                    errors.password ? 'border-utility-500 focus:ring-utility-500 focus:border-utility-500' : 'focus:ring-utility-500 focus:border-utility-500'
                   }`}
                 />
                 {errors.password && (
-                  <p className="mt-1 text-sm text-red-600 dark:text-red-500">{errors.password}</p>
+                  <p className="mt-1 text-sm text-utility-600 dark:text-utility-500">{errors.password}</p>
                 )}
               </div>
               <div className="h-[85px]">
-                <label htmlFor="password_confirmation" className="block mb-2 text-sm font-medium text-zinc-900 dark:text-white">Confirm password</label>
+                <label htmlFor="password_confirmation" className="block mb-2 text-sm font-medium text-neutral-900 dark:text-white">Confirm password</label>
                 <input 
                   type="password" 
                   name="password_confirmation"
                   id="password_confirmation" 
                   onChange={(e) => handleChange('password_confirmation', e.target.value)} 
                   placeholder="••••••••" 
-                  className={`bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg block w-full p-2.5 dark:bg-zinc-700 dark:text-white dark:border-zinc-700 ${
-                    errors.password_confirmation ? 'border-red-500 focus:ring-red-500 focus:border-red-500' : 'focus:ring-red-500 focus:border-red-500'
+                  className={`bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg block w-full p-2.5 dark:bg-neutral-700 dark:text-white dark:border-neutral-700 ${
+                    errors.password_confirmation ? 'border-utility-500 focus:ring-utility-500 focus:border-utility-500' : 'focus:ring-utility-500 focus:border-utility-500'
                   }`}
                 />
                 {errors.password_confirmation && (
-                  <p className="mt-1 text-sm text-red-600 dark:text-red-500">{errors.password_confirmation}</p>
+                  <p className="mt-1 text-sm text-utility-600 dark:text-utility-500">{errors.password_confirmation}</p>
                 )}
               </div>
               <button
                 type="submit"
                 disabled={processing}
-                className="w-full text-white bg-red-700 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-800 disabled:opacity-75">
+                className="w-full text-white bg-utility-700 hover:bg-utility-800 focus:ring-2 focus:outline-none focus:ring-utility-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 text-center dark:bg-utility-500 dark:hover:bg-utility-600 dark:focus:ring-utility-800 disabled:opacity-75">
                 {processing ? 'Resetting...' : 'Reset password'}
               </button>
             </form>
